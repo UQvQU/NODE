0aa749e876ffd53f96d8e63aeafda5a6eef3d4d4
TT <2991369095@qq.com>
2020-11-06 09:56:21 +0800

    webpack配置文件打包

--- webpack/demo2/webpack.config.js
+++ webpack/demo2/webpack.config.js
@@ -1,3 +1,21 @@
+let path = require('path')
+console.log(path.resolve(__dirname, 'dist'))
+
 module.exports = {
   // 入口文件
+  entry: './src/index.js',
+  output: {
+    // 输出文件名称
+    filename: 'bundle.js',
+    // 输出的路径
+    // 绝对路径
+    path: path.resolve(__dirname, 'dist')
+  },
+  // 开发模式   production（生产模式）
+  mode: 'development'
 }
+
+/**
+ * 使用webpack配置文件打包
+ * 打包命名：webpack
+ */
