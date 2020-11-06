6b4fb9b2808a7f8a36606f63b6b141f0a166ca86
TT <2991369095@qq.com>
2020-11-06 09:55:17 +0800

    webpack命令打包文件

--- webpack/demo1/src/index.js
+++ webpack/demo1/src/index.js
@@ -5,9 +5,11 @@ function fn1(data) {
 fn1(data)
 
 /**
+ * npm i -y  初始化
+ * 
  * 开发环境打包命令： webpack 入口文件 -o  输出目录  --mode=development（模式）
 webpack ./src/index.js -o ./dist/development --mode=development
-生产环境打包：
+生产环境打包命令：
 webpack ./src/index.js -o ./dist/production --mode=production
 
 webpack默认可以压缩js文件和json文件
